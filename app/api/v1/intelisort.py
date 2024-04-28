1495e4a648ba229e6c1a598fee9a8bc10bf36bb0
MoosaTae <sadhis.tae@gmail.com>
2024-04-28 21:42:51 +0700

    format code

--- app/api/v1/intelisort.py
+++ app/api/v1/intelisort.py
@@ -1,5 +1,13 @@
 import sys
-from fastapi import APIRouter, Request, status, File, UploadFile, BackgroundTasks, HTTPException
+from fastapi import (
+    APIRouter,
+    Request,
+    status,
+    File,
+    UploadFile,
+    BackgroundTasks,
+    HTTPException,
+)
 from fastapi.responses import JSONResponse
 from dotenv import load_dotenv
 from loguru import logger
@@ -12,7 +20,7 @@ import pandas as pd
 import numpy as np
 from pythainlp.tokenize import word_tokenize
 import pythainlp
-from app.function.helper import * 
+from app.function.helper import *
 import io
 import csv
 import codecs
@@ -21,31 +29,44 @@ redis = None
 
 # define logger format
 logger.remove(0)
-logger.add(sys.stderr, format="{time:MMMM D, YYYY > HH:mm:ss!UTC} | {level} | {message}", serialize=False)
+logger.add(
+    sys.stderr,
+    format="{time:MMMM D, YYYY > HH:mm:ss!UTC} | {level} | {message}",
+    serialize=False,
+)
+
 
 async def startup_event():
     logger.info("Connecting to database...")
     global redis
-    redis = await aioredis.from_url(os.environ.get("REDISCLOUD_URL", "redis://localhost"))  # Default to localhost if env var is missing
+    redis = await aioredis.from_url(
+        os.environ.get("REDISCLOUD_URL", "redis://localhost")
+    )  # Default to localhost if env var is missing
     logger.info("Loading csv into Redis")
     logger.info(os.getcwd())
     await load_csv_to_redis(redis=redis)
     logger.info("Kumyarb words loaded into Redis")
+
 
 # Define your shutdown event handler
 async def shutdown_event():
     logger.info("Disconnecting from database...")
     await redis.close()
 
+
 router = APIRouter()
 # Attach the event handlers to the FastAPI app
 router.add_event_handler("startup", startup_event)
 router.add_event_handler("shutdown", shutdown_event)
 
+
 # -- Routes --
-@router.get('/', tags=["Health Check"])
+@router.get("/", tags=["Health Check"])
 async def health_check(request: Request) -> base_response.BaseStatusResponseModel:
-    return base_response.BaseStatusResponseModel(success=True, content="Intelisort API is up and running!")
+    return base_response.BaseStatusResponseModel(
+        success=True, content="Intelisort API is up and running!"
+    )
+
 
 # upload CSV data
 from starlette.status import HTTP_409_CONFLICT
@@ -53,38 +74,61 @@ from starlette.status import HTTP_409_CONFLICT
 processing_lock = False
 chunk_counter = 0
 
-@router.post('/import/csv', tags=["import data (Under Development)"])
+
+@router.post("/import/csv", tags=["import data (Under Development)"])
 async def import_csv(
     background_tasks: BackgroundTasks,
     csv_file: UploadFile = File(...),
 ) -> base_response.BaseStatusResponseModel:
     global processing_lock, chunk_counter
     if processing_lock:
-        raise HTTPException(status_code=HTTP_409_CONFLICT, detail="Another file is already being processed. Please try again later.")
-    
+        raise HTTPException(
+            status_code=HTTP_409_CONFLICT,
+            detail="Another file is already being processed. Please try again later.",
+        )
+
     processing_lock = True
     chunk_counter = 0
-    
+
     logger.info(f"Uploading file: {csv_file.filename}")
     valid_columns = [
-        'ticket_id', 'type', 'organization', 'comment', 'coords', 'photo', 'photo_after',
-        'address', 'subdistrict', 'district', 'province', 'timestamp', 'state', 'star',
-        'count_reopen', 'last_activity'
+        "ticket_id",
+        "type",
+        "organization",
+        "comment",
+        "coords",
+        "photo",
+        "photo_after",
+        "address",
+        "subdistrict",
+        "district",
+        "province",
+        "timestamp",
+        "state",
+        "star",
+        "count_reopen",
+        "last_activity",
     ]
     try:
-        if not csv_file.filename.endswith('.csv'):
-            raise TypeError(f"Invalid file type, only accept .csv file, but got {csv_file.filename}")
-        csv_content = csv.reader(codecs.iterdecode(csv_file.file, 'utf-8-sig'))
+        if not csv_file.filename.endswith(".csv"):
+            raise TypeError(
+                f"Invalid file type, only accept .csv file, but got {csv_file.filename}"
+            )
+        csv_content = csv.reader(codecs.iterdecode(csv_file.file, "utf-8-sig"))
         header = next(csv_content)
         if not all([column in valid_columns for column in header]):
-            raise ValueError(f"Invalid file column, Required: [{', '.join(valid_columns)}] but got [{', '.join(header)}]")
+            raise ValueError(
+                f"Invalid file column, Required: [{', '.join(valid_columns)}] but got [{', '.join(header)}]"
+            )
         data_json = []
         chunk_size = 200
         for row in csv_content:
             data_json.append(dict(zip(header, row)))
             if len(data_json) == chunk_size:
                 chunk_counter += 1
-                background_tasks.add_task(process_chunk, data_json, redis, chunk_counter)
+                background_tasks.add_task(
+                    process_chunk, data_json, redis, chunk_counter
+                )
                 data_json = []
         if data_json:
             chunk_counter += 1
@@ -93,15 +137,21 @@ async def import_csv(
     except Exception as e:
         logger.error(f"Error reading file: {str(e)}")
         processing_lock = False
-        return base_response.BaseStatusResponseModel(success=False, status=f"Error reading file: {str(e)}")
-    
+        return base_response.BaseStatusResponseModel(
+            success=False, status=f"Error reading file: {str(e)}"
+        )
+
     background_tasks.add_task(complete_processing, redis)
-    return base_response.BaseStatusResponseModel(success=True, status="CSV file processing started in the background")
+    return base_response.BaseStatusResponseModel(
+        success=True, status="CSV file processing started in the background"
+    )
+
 
 async def process_chunk(data_chunk, redis, chunk_number):
     logger.info(f"Processing data chunk: {chunk_number}")
     await batch_add_data(data_chunk, redis)
     await generate_embeddings_redis(redis)
+
 
 async def complete_processing(redis):
     global processing_lock
@@ -111,32 +161,29 @@ async def complete_processing(redis):
     await drop_index(redis)
     processing_lock = False
 
+
 # Query data
-@router.post('/query', tags=["query data (Under Development)"])
+@router.post("/query", tags=["query data (Under Development)"])
 async def query_data(query: list[str]) -> query.QueryResponseModel:
     result = query_all_texts(query, top_k=5)
     return query.QueryResponseModel(success=True, content=result)
 
-@router.post('/curse_check', tags=["Functionality"])
+
+@router.post("/curse_check", tags=["Functionality"])
 async def curse_check(text: list[str]):
     result = []
     for t in text:
         meta, new_text = await check_kumyarb(t)
-        result.append(
-            {
-                "original": t,
-                "censored": new_text,
-                "meta": meta
-            }
-        )
-    
+        result.append({"original": t, "censored": new_text, "meta": meta})
+
     return kumyarb.KumYarbResponseModel(success=True, content=result)
+
 
 # -- Function --
 async def load_csv_to_redis(redis):
     df = pd.read_csv("app/api/v1/static/kumyarb.csv")
     # using HIGH MID LOW
-    for level in ['HIGH', 'MID', 'LOW']:
+    for level in ["HIGH", "MID", "LOW"]:
         key = f"words:{level.lower()}"  # Redis key pattern, e.g., "words:high"
         values = df[level].dropna().tolist()
         if values:
@@ -148,26 +195,14 @@ async def check_kumyarb(text):
     new_text = ""
     for i, v in enumerate(word_tokenize(text, engine="newmm")):
         # Check membership in the Redis sets
-        if await redis.sismember('words:high', v):
-            arr.append({
-                "word_num": i,
-                "word": v,
-                "severity": 'HIGH'
-            })
+        if await redis.sismember("words:high", v):
+            arr.append({"word_num": i, "word": v, "severity": "HIGH"})
             new_text += "xxx"
-        elif await redis.sismember('words:mid', v):
-            arr.append({
-                "word_num": i,
-                "word": v,
-                "severity": 'MID'
-            })
+        elif await redis.sismember("words:mid", v):
+            arr.append({"word_num": i, "word": v, "severity": "MID"})
             new_text += "xxx"
-        elif await redis.sismember('words:low', v):
-            arr.append({
-                "word_num": i,
-                "word": v,
-                "severity": 'LOW'
-            })
+        elif await redis.sismember("words:low", v):
+            arr.append({"word_num": i, "word": v, "severity": "LOW"})
             new_text += "xxx"
         else:
             new_text += v
--- app/function/helper.py
+++ app/function/helper.py
@@ -1,4 +1,10 @@
-from app.config.config import TEXT_KEY_NAME, EMBEDDING_KEY_NAME, TEXT_INDEX_NAME, PREFIX_INDEX_KEY, VECTOR_DIMENSION
+from app.config.config import (
+    TEXT_KEY_NAME,
+    EMBEDDING_KEY_NAME,
+    TEXT_INDEX_NAME,
+    PREFIX_INDEX_KEY,
+    VECTOR_DIMENSION,
+)
 import numpy as np
 from redis.commands.search.field import (
     NumericField,
@@ -13,9 +19,11 @@ import modal
 from typing import Literal
 import base64
 from dotenv import load_dotenv
+
 load_dotenv()
 
 # Helper functions
+
 
 def call_sentence_encoder(sentences: list[str]) -> list[list[float]]:
     """
@@ -31,9 +39,10 @@ def call_sentence_encoder(sentences: list[str]) -> list[list[float]]:
         )
         result: list[list[float]] = inf_fx.remote(sentences)
         return result
-    
+
     except Exception as e:  # will be raised from modal if inf_fx.remote fails
         raise e
+
 
 def replace_nan_with_empty_string(obj):
     if isinstance(obj, dict):
@@ -46,58 +55,64 @@ def replace_nan_with_empty_string(obj):
         return ""
     return obj
 
-def preprocess_prompt_dict(data : dict) -> str:
-    data['type'] = data['type'].strip('{}')
-    data['type'] = "ไม่มีชนิด" if data['type'] == "" else data['type']
-    
-    data['comment'] = data['comment'].replace('ปัญหา:', '')
-    return data['type'] + ' ' + data['comment']
 
-def preprocess_raw_data(data : dict) -> dict:
-    data = {k : v if v is not None else "" for k, v in data.items()}
+def preprocess_prompt_dict(data: dict) -> str:
+    data["type"] = data["type"].strip("{}")
+    data["type"] = "ไม่มีชนิด" if data["type"] == "" else data["type"]
+
+    data["comment"] = data["comment"].replace("ปัญหา:", "")
+    return data["type"] + " " + data["comment"]
+
+
+def preprocess_raw_data(data: dict) -> dict:
+    data = {k: v if v is not None else "" for k, v in data.items()}
     data = replace_nan_with_empty_string(data)
-    
+
     return data
+
 
 async def preprocess_and_store_data(data, r):
     data = preprocess_raw_data(data)
     preprocess_prompt = preprocess_prompt_dict(data)
 
     data_key = f"{TEXT_KEY_NAME}:{data['ticket_id']}"
-    pipeline = r.pipeline(transaction = False)
-    preprocess_prompt = {
-        "raw_data" : data,
-        "preprocess_prompt" : preprocess_prompt
-    }
-    await pipeline.json().set(data_key, '$', preprocess_prompt)
-    
+    pipeline = r.pipeline(transaction=False)
+    preprocess_prompt = {"raw_data": data, "preprocess_prompt": preprocess_prompt}
+    await pipeline.json().set(data_key, "$", preprocess_prompt)
+
     await pipeline.execute()
+
 
 async def store_embeddings(key, embeddings, r):
-    pipeline = r.pipeline(transaction = False)
+    pipeline = r.pipeline(transaction=False)
     for embedding in embeddings:
-        await pipeline.json().set(key, '$', embedding)
+        await pipeline.json().set(key, "$", embedding)
     await pipeline.execute()
 
-async def add_data(data : dict, r):
+
+async def add_data(data: dict, r):
     await preprocess_and_store_data(data, r)
 
-async def batch_add_data(data : list[dict], r):
+
+async def batch_add_data(data: list[dict], r):
     for d in data:
         await preprocess_and_store_data(d, r)
+
 
 # Database helper
 async def clear_database(r):
     """Clear the Redis database."""
     await r.flushall()
 
+
 async def drop_index(r):
-    drop_index_command = ["FT.DROPINDEX", TEXT_INDEX_NAME] #, -DD] 
+    drop_index_command = ["FT.DROPINDEX", TEXT_INDEX_NAME]  # , -DD]
     await r.execute_command(*drop_index_command)
 
-#TODO : batch inference
+
+# TODO : batch inference
 async def generate_embeddings_redis(r):
-    keys = await r.keys(f'{TEXT_KEY_NAME}:*')
+    keys = await r.keys(f"{TEXT_KEY_NAME}:*")
     keys = sorted(keys)
     texts = await r.json().mget(keys, "$.preprocess_prompt")
     texts = [t for sublist in texts for t in sublist]
@@ -110,43 +125,52 @@ async def generate_embeddings_redis(r):
 
     await pipeline.execute()
 
+
 # index helper
 async def create_index_text(r):
     try:
         schema = (
             # TextField('$.preprocess_prompt', no_stem=True, as_name='preprocess_prompt'),
-            TextField("$.raw_data.state",no_stem=True, as_name="state"),
-            TextField("$.raw_data.comment",no_stem=True, as_name="comment"),
-            TextField("$.raw_data.type",no_stem=True, as_name="type"),
-            TextField("$.raw_data.address",no_stem=True, as_name="address"),
-            TextField("$.raw_data.district",no_stem=True, as_name="district"),
-            TextField("$.raw_data.province",no_stem=True, as_name="province"),
-            TextField("$.raw_data.subdistrict",no_stem=True, as_name="subdistrict"),
-            VectorField(f'$.{EMBEDDING_KEY_NAME}', 'FLAT', {
-                'TYPE': 'FLOAT32',
-                'DIM': VECTOR_DIMENSION,
-                'DISTANCE_METRIC': 'COSINE'
-            }, as_name='vector')
+            TextField("$.raw_data.state", no_stem=True, as_name="state"),
+            TextField("$.raw_data.comment", no_stem=True, as_name="comment"),
+            TextField("$.raw_data.type", no_stem=True, as_name="type"),
+            TextField("$.raw_data.address", no_stem=True, as_name="address"),
+            TextField("$.raw_data.district", no_stem=True, as_name="district"),
+            TextField("$.raw_data.province", no_stem=True, as_name="province"),
+            TextField("$.raw_data.subdistrict", no_stem=True, as_name="subdistrict"),
+            VectorField(
+                f"$.{EMBEDDING_KEY_NAME}",
+                "FLAT",
+                {
+                    "TYPE": "FLOAT32",
+                    "DIM": VECTOR_DIMENSION,
+                    "DISTANCE_METRIC": "COSINE",
+                },
+                as_name="vector",
+            ),
         )
     except:
         await r.ft(TEXT_INDEX_NAME).info()
         print(f"Index {TEXT_INDEX_NAME} already exists")
-        
+
     definition = IndexDefinition(prefix=[PREFIX_INDEX_KEY], index_type=IndexType.JSON)
 
     await r.ft(TEXT_INDEX_NAME).create_index(fields=schema, definition=definition)
 
+
 async def get_info_index(r):
     info = await r.ft(TEXT_INDEX_NAME).info()
 
-    num_docs = info['num_docs']
-    indexing_failures = info['hash_indexing_failures']
-    total_indexing_time = info['total_indexing_time']
-    percent_indexed = float(info['percent_indexed']) * 100
+    num_docs = info["num_docs"]
+    indexing_failures = info["hash_indexing_failures"]
+    total_indexing_time = info["total_indexing_time"]
+    percent_indexed = float(info["percent_indexed"]) * 100
 
-    return (f"{num_docs} docs ({percent_indexed}%) indexed w/ {indexing_failures} failures in {float(total_indexing_time):.2f} msecs")
+    return f"{num_docs} docs ({percent_indexed}%) indexed w/ {indexing_failures} failures in {float(total_indexing_time):.2f} msecs"
+
 
 # query helper
+
 
 async def query_all_embeddings(r, embeddings, top_k=5):
     results_list = []
